aed9839058081afb03adfda44e12f48dd8b4f7bf
Assem Talha <astalha@e2r4p8.1337.ma>
2023-01-23 19:50:07 +0100

    fixing some staff

--- bonus/so_long_bonus.c
+++ bonus/so_long_bonus.c
@@ -6,7 +6,7 @@
 /*   By: astalha <astalha@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 19:23:35 by astalha           #+#    #+#             */
-/*   Updated: 2023/01/22 22:46:37 by astalha          ###   ########.fr       */
+/*   Updated: 2023/01/23 19:43:19 by astalha          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,14 @@ int	main(int ac, char **av)
 	my_infos.matrix = matrix(ac, av, &my_infos);
 	if (!my_infos.matrix)
 		return (exit(0), 0);
+	if ((int)ft_strlen(my_infos.matrix[0]) * 64 > 5120
+		|| matlen(my_infos.matrix) * 64 > 2880)
+	{
+		ft_putstr_fd("Error : map has depassed the display", 2);
+		ft_putstr_fd(" limits of this screen 5120 x 2880", 2);
+		free(my_infos.matrix);
+		exit(0);
+	}
 	if (!valid_path(&my_infos))
 		return (freealloc2(my_infos.matrix),
 			freealloc2(my_infos.copy), exit(0), 0);
